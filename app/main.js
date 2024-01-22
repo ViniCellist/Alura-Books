57d9d2abc184f6a5b9d618887ad5b7f6dd522bcd
Vinicius Duarte <vsduartestudy@outlook.com>
2024-01-22 18:51:48 -0300

    feat: dynamic book list at screen

--- app/main.js
+++ app/main.js
@@ -2,10 +2,30 @@ let books = []
 const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 
 getBooksFromAPI()
+const insertElementBooks = document.getElementById('livros')
 
 async function getBooksFromAPI() {
     const res = await fetch(endpointAPI)
     books = await res.json()
 
     console.table(books)
+    showBooksInScreen(books)
+}
+
+function showBooksInScreen(bookList) {
+    bookList.forEach(books => {
+        insertElementBooks.innerHTML += `
+        <div class="livro">
+            <img class="livro__imagens" src="${books.imagem}" alt="${books.alt}" />
+            <h2 class="livro__titulo">
+                ${books.titulo}
+            </h2>
+            <p class="livro__descricao">${books.autor}</p>
+            <p class="livro__preco" id="preco">R$${books.preco}</p>
+            <div class="tags">
+                <span class="tag">${books.categoria}</span>
+            </div>
+        </div>
+        `
+    })
 }
