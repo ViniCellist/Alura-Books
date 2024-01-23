27ecf7d829762792a69d2e14e4bb2ed7b34bd9d6
Vinicius Duarte <vsduartestudy@outlook.com>
2024-01-23 16:53:20 -0300

    refactor: separeted forEach from main

---
+++ app/forEachMethod.js
@@ -0,0 +1,19 @@
+const insertElementBooks = document.getElementById('livros')
+
+function showBooksInScreen(bookList) {
+    bookList.forEach(books => {
+        insertElementBooks.innerHTML += `
+        <div class="livro">
+            <img class="livro__imagens" src="${books.imagem}" alt="${books.alt}" />
+            <h2 class="livro__titulo">
+                ${books.titulo}
+            </h2>
+            <p class="livro__descricao">${books.autor}</p>
+            <p class="livro__preco" id="preco">R$${books.preco.toFixed(2)}</p>
+            <div class="tags">
+                <span class="tag">${books.categoria}</span>
+            </div>
+        </div>
+        `
+    })
+}
