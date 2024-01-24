bbf1123a8dc74fb5d1008bcc704037ff0ba6cafe
Vinicius Duarte <vsduartestudy@outlook.com>
2024-01-24 19:53:51 -0300

    refactor: books -> book

--- app/forEachMethod.js
+++ app/forEachMethod.js
@@ -2,17 +2,17 @@ const insertElementBooks = document.getElementById('livros')
 
 function showBooksInScreen(bookList) {
     insertElementBooks.innerHTML = ''
-    bookList.forEach(books => {
+    bookList.forEach(book => {
         insertElementBooks.innerHTML += `
         <div class="livro">
-            <img class="livro__imagens" src="${books.imagem}" alt="${books.alt}" />
+            <img class="livro__imagens" src="${book.imagem}" alt="${book.alt}" />
             <h2 class="livro__titulo">
-                ${books.titulo}
+                ${book.titulo}
             </h2>
-            <p class="livro__descricao">${books.autor}</p>
-            <p class="livro__preco" id="preco">R$${books.preco.toFixed(2)}</p>
+            <p class="livro__descricao">${book.autor}</p>
+            <p class="livro__preco" id="preco">R$${book.preco.toFixed(2)}</p>
             <div class="tags">
-                <span class="tag">${books.categoria}</span>
+                <span class="tag">${book.categoria}</span>
             </div>
         </div>
         `
