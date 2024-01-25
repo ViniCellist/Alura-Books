9dc40c8037d89cd6a8e9f295a967e1aa45e93539
Vinicius Duarte <vsduartestudy@outlook.com>
2024-01-25 20:08:41 -0300

    refactor: show books logic

--- app/filterMethod.js
+++ app/filterMethod.js
@@ -4,6 +4,6 @@ btns.forEach(btn => btn.addEventListener('click', filterBooks))
 function filterBooks() {
     const elementBtn = document.getElementById(this.id)
     const category = elementBtn.value
-    let filteredBooks = books.filter(book => book.categoria == category)
+    let filteredBooks = category == 'disponivel' ? books.filter(book => book.quantidade > 0) : books.filter(book => book.categoria == category)
     showBooksInScreen(filteredBooks)
 }
