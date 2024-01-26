4425962d18249d6a64ff7e9c77d3d383548c73c3
Vinicius Duarte <vsduartestudy@outlook.com>
2024-01-26 15:44:07 -0300

    refactor: better view

--- app/filterMethod.js
+++ app/filterMethod.js
@@ -1,9 +1,28 @@
-const btns = document.querySelectorAll('.btn')
-btns.forEach(btn => btn.addEventListener('click', filterBooks))
+const btns = document.querySelectorAll('.btn');
+btns.forEach(btn => btn.addEventListener('click', filterBooks));
 
 function filterBooks() {
-    const elementBtn = document.getElementById(this.id)
-    const category = elementBtn.value
-    let filteredBooks = category == 'disponivel' ? books.filter(book => book.quantidade > 0) : books.filter(book => book.categoria == category)
-    showBooksInScreen(filteredBooks)
+    const elementBtn = document.getElementById(this.id);
+    const category = elementBtn.value;
+    let filteredBooks = category == 'disponivel' ? filterAvailability() : filterCategory(category);
+    showBooksInScreen(filteredBooks);
+    if (category == 'disponivel') {
+        showTotalAvailableBooks()
+    };
+};
+
+function filterCategory(category) {
+    return books.filter(book => book.categoria == category);
 }
+
+function filterAvailability() {
+    return books.filter(book => book.quantidade > 0);
+};
+
+function showTotalAvailableBooks() {
+    elementTotalAvailableBooks.innerHTML = `
+        <div class="livros__disponiveis">
+            <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+        </div>
+    `
+};
