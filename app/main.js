273ff63b67f90d7c031904aae6447242b04a50d7
Vinicius Duarte <vsduartestudy@outlook.com>
2024-01-29 15:38:28 -0300

    feat: adding ;

--- app/main.js
+++ app/main.js
@@ -1,12 +1,12 @@
 let books = [];
 const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 
-getBooksFromAPI()
+getBooksFromAPI();
 
 async function getBooksFromAPI() {
     const res = await fetch(endpointAPI);
     books = await res.json();
     let discountBooks = applyDiscount(books);
     showBooksInScreen(discountBooks);
-}
+};
 
